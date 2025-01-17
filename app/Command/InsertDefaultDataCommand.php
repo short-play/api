71783cfe21e19273e61d09f058ff188b5dda3577
Your Name <you@example.com>
2025-01-17 23:03:37 +0800

    update

--- app/Command/InsertDefaultDataCommand.php
+++ app/Command/InsertDefaultDataCommand.php
@@ -12,28 +12,11 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Model\Admin;
-use App\Model\BackstageRecharge;
-use App\Model\DealCountResetRecord;
-use App\Model\Good;
 use App\Model\MessageCount;
-use App\Model\Order;
-use App\Model\Permission;
-use App\Model\Role;
-use App\Model\Setting;
 use App\Model\User;
 use App\Model\UserCoin;
-use App\Model\UserCommissionRecord;
-use App\Model\UserLevel;
-use App\Model\UserOrderContinualItem;
-use App\Model\UserOrderContinuous;
-use App\Model\UserRecharge;
-use App\Model\UserWalletLog;
-use App\Model\UserWithdrawalInfo;
-use App\Model\UserWithdrawCash;
 use Hyperf\Command\Annotation\Command;
 use Hyperf\Command\Command as HyperfCommand;
-use Hyperf\DbConnection\Db;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\Filesystem\FilesystemFactory;
 use function Hyperf\Support\now;
